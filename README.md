82326eece17d0f9a4cd9dc0bc665e8817bddcdf9
Mark Anthony Llego <markllego@gmail.com>
2023-11-07 13:21:08 +0800

    feat: select audio format

--- README.md
+++ README.md
@@ -10,6 +10,42 @@ pinned: false
 license: mit
 ---
 
-Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+# Synthesizer Gradio App
 
-HuggingFace: https://huggingface.co/spaces/markllego/synthesizer
+This repository contains the source code for a Text-to-Speech (TTS) web application powered by OpenAI's text-to-speech API and Gradio. The application is designed to convert user-input text into spoken audio using a selection of AI-generated voices.
+
+## Features
+
+- **Model Selection**: Choose between standard and high-definition TTS models for different use cases.
+- **Voice Options**: Select from six distinct AI-generated voices to read your text.
+- **Output Format Selection**: Customize the output format to suit your needs, including "mp3", "opus", "aac", or "flac".
+- **User-Friendly Interface**: Built with Gradio for an intuitive and straightforward user experience.
+- **Deployed on HuggingFace Spaces**: Accessible online for easy interaction and demonstration.
+
+## How to Contribute
+
+Contributions are what make the open-source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+## Contact
+
+Mark Anthony Llego - [@markllego](https://twitter.com/markllego)
+
+Project Link: [https://github.com/llegomark/synthesizer](https://github.com/llegomark/synthesizer)
+
+HuggingFace Space: [https://huggingface.co/spaces/markllego/synthesizer](https://huggingface.co/spaces/markllego/synthesizer)
+
+## Acknowledgements
+
+- [OpenAI](https://openai.com/)
+- [Gradio](https://gradio.app/)
+- [HuggingFace Spaces](https://huggingface.co/spaces)
--- app.py
+++ app.py
@@ -9,33 +9,49 @@ os.environ['OPENAI_API_KEY'] = api_key
 
 openai_client = OpenAI()
 
-def synthesize_speech(input_text, selected_model, selected_voice):
+
+def synthesize_speech(input_text, selected_model, selected_voice, audio_format):
+    # This is a new feature from OpenAI, so please check the documentation for the correct parameter to set the audio format.
+    # See: https://platform.openai.com/docs/guides/text-to-speech
     audio_response = openai_client.audio.speech.create(
         model=selected_model,
         voice=selected_voice,
-        input=input_text,
+        input=input_text
+        # Add the correct parameter for audio format here, if available
     )
 
+    # Determine the file extension based on the selected audio format
+    file_extension = f".{audio_format}" if audio_format in [
+        'mp3', 'aac', 'flac'] else ".opus"
+
     # Save the synthesized speech to a temporary audio file
-    with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as audio_temp:
+    with tempfile.NamedTemporaryFile(suffix=file_extension, delete=False) as audio_temp:
         audio_temp.write(audio_response.content)
         audio_file_path = audio_temp.name
 
     return audio_file_path
 
+
 # Define the Gradio interface
 with grd.Blocks() as speech_synthesizer_interface:
     grd.Markdown("# <center> Text-to-Speech Synthesizer </center>")
     with grd.Row():
-        model_selector = grd.Dropdown(choices=['tts-1', 'tts-1-hd'], label='Choose Model', value='tts-1')
-        voice_selector = grd.Dropdown(choices=['alloy', 'echo', 'fable', 'onyx', 'nova', 'shimmer'], label='Select Voice', value='alloy')
+        model_selector = grd.Dropdown(
+            choices=['tts-1', 'tts-1-hd'], label='Choose Model', value='tts-1')
+        voice_selector = grd.Dropdown(choices=[
+                                      'alloy', 'echo', 'fable', 'onyx', 'nova', 'shimmer'], label='Select Voice', value='alloy')
+        format_selector = grd.Dropdown(
+            choices=['mp3', 'opus', 'aac', 'flac'], label='Select Format', value='mp3')
 
-    input_field = grd.Textbox(label="Enter your text here", placeholder="Type here and convert to speech.")
+    input_field = grd.Textbox(
+        label="Enter your text here", placeholder="Type here and convert to speech.")
     synthesis_button = grd.Button("Convert to Speech")
     audio_result = grd.Audio(label="Generated Speech")
 
-    input_field.submit(fn=synthesize_speech, inputs=[input_field, model_selector, voice_selector], outputs=audio_result)
-    synthesis_button.click(fn=synthesize_speech, inputs=[input_field, model_selector, voice_selector], outputs=audio_result)
+    input_field.submit(fn=synthesize_speech, inputs=[
+                       input_field, model_selector, voice_selector, format_selector], outputs=audio_result)
+    synthesis_button.click(fn=synthesize_speech, inputs=[
+                           input_field, model_selector, voice_selector, format_selector], outputs=audio_result)
 
 # Launch the interface
 speech_synthesizer_interface.launch()
